f12c480e83b5d935df87eaaebef12af96d7ce3b9
DanilaNesterko <54835563+DanilaNesterko@users.noreply.github.com>
2020-03-20 12:41:53 +0200

    Add files via upload

---
+++ Heroes/server.js
@@ -0,0 +1,8 @@
+var superheroes = require('superheroes');
+var supervillains = require('supervillains');
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+var AllSuperVillian = supervillains.all;
+//console.log(mySuperheroName);
+console.log(mySupervillainName);
+console.log("All Villain:", AllSuperVillian);
